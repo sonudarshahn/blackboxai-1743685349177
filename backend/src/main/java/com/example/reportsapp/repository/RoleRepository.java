2966088fc2564d46ea3a3b5e2048c77e7d5cf224
sonudarshahn <152383769+sonudarshahn@users.noreply.github.com>
2025-04-03 18:35:44 +0530

    Add backend/src/main/java/com/example/reportsapp/repository/RoleRepository.java 1743685379811

---
+++ backend/src/main/java/com/example/reportsapp/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.example.reportsapp.repository;
+
+import com.example.reportsapp.entity.ERole;
+import com.example.reportsapp.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(ERole name);
+}
